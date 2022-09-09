6ee9da878fcd2c7a40d88f079fc652c7010eeabd
Stanislav Stefanov <56052683+stefanovstan@users.noreply.github.com>
2022-09-09 11:42:17 -0400

    Add card tests and getDigits function

--- Lab1/Excercise7.hs
+++ Lab1/Excercise7.hs
@@ -4,6 +4,10 @@ import Test.QuickCheck
 luhn :: Integer -> Bool
 luhn n = compareChecksums $ convertIntToIntArray n
 
+getDigits :: Integral x => x -> [x]
+getDigits 0 = []
+getDigits x = getDigits (x `div` 10) ++ [x `mod` 10]
+
 compareChecksums :: [Int] -> Bool
 compareChecksums a = getPayloadChecksum (take (length a - 1) a) == last a
 
@@ -31,3 +35,12 @@ makeWeightedProducts a = zipWith (*) a (makeWeights a)
 -- Make an array of weights where every other digit is 2 starting from the right
 makeWeights :: [Int] -> [Int]
 makeWeights a = reverse $ take (length a) (cycle [2, 1])
+
+isAmericanExpress :: Integer -> Bool
+isAmericanExpress n = (luhn n) && length (show n) == 15 && (take 2 (getDigits n) == [3, 4] || take 2 (getDigits n) == [3, 7])
+
+isMaster :: Integer -> Bool
+isMaster n = (luhn n) && length (show n) == 16 && ((read (take 2 (show n))) >= 51 && (read (take 2 (show n))) <= 55 ) || ((read (take 2 (show n))) >= 2221 && (read (take 2 (show n))) <= 2720 )
+
+isVisa :: Integer -> Bool
+isVisa n = (luhn n) && (length (show n) == 16 || length (show n) == 13) && (take 1 (getDigits n) == [4])
