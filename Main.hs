35b26c9ae1114fd00fc575d5c5b8fa9ab785ea4b
Selin99 <48292786+Selin99@users.noreply.github.com>
2022-09-15 13:34:14 +0200

    soln for Ex3

--- Main.hs
+++ Main.hs
@@ -4,12 +4,18 @@ import Test.QuickCheck
 import Data.List
 import System.Random
 import System.IO
+import Data.Function
 
 --Testing QuickCheck
-
 prop_reverse :: [Int] -> Bool
 prop_reverse xs = reverse ( reverse xs) == xs
 
+--Property for -->
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+forall = flip all
 -----------------------------------EXERCISE 1 STARTS-----------------------------------------
 --Monads do not store the values or do not evaluate the values, they just display the outcome
 probs :: Int -> IO [Float]
@@ -38,7 +44,7 @@ func1 n = do
 
 
 --Step 3: We count number of times each element occurs in a list
-numTimesFound x xs = (length . filter (== x)) xs
+numTimesFound x xs = (length. filter (== x)) xs
 
 
 
@@ -82,28 +88,55 @@ prop4 :: Int -> Bool
 prop4 n = (((n `mod` 2) == 0) && (n>3) ) || ((n `mod` 2) == 0) 
 
 --pre-condition : p (-10<= x <=10)
---p:: a-> Bool
---p x =(x>= (-10)) && (x<10)
+p :: [Int]
+p = [-10..10]
 
---post-condition : q 
---q:: a-> Bool
+--Get output space of prop1:
+test1 = length (filter (\x-> x==True) (map (prop1) p))
+--Get output space of prop2:
+test2 = length (filter (\x-> x==True) (map (prop2) p))
+--Get output space of prop3:
+test3 = length (filter (\x-> x==True) (map (prop3) p))
+--Get output space of prop4:
+test4 = length (filter (\x-> x==True) (map (prop4) p))
 
---p x =(x>= (-10)) && (x<10)
 
---stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
---stronger xs p q = all xs (\ x -> p x --> q x)
---weaker   xs p q = stronger xs q p 
+stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
+stronger xs p q = forall xs (\ x -> p x --> q x)
+weaker   xs p q = stronger xs q p 
 
+type Poly = [(Float,Int)]
+
+mySort :: Ord b => [(a, b)] -> [(a, b)]
+mySort = sortBy (flip compare `on` snd)
 -----------------------------------EXERCISE 3 ENDS-----------------------------------------
+-----------------------------------EXERCISE 4 STARTS-----------------------------------------
+perms :: Eq a => [a] -> [[a]]
+perms [] = [[]]
+perms xs = [ i:j | i <- xs, j <- perms $ delete i xs ]
+
+--isPermutation :: Eq a => [a] -> [a] -> Bool
+
+-----------------------------------EXERCISE 4 ENDS-----------------------------------------
 main :: IO ()
 main = do
     ---- CALL FOR EXERCISE 1 ---------
     var <- probs 1000 -- we unwrapped IO float to [Float] using <-
-    print(var) --List created randomly each time by probs func
+    --print(var) --List created randomly each time by probs func
 
     print(numTimesFound "Interval 1" (func2 $ var))
     print(numTimesFound "Interval 2" (func2 $ var))
     print(numTimesFound "Interval 3" (func2 $ var))
     print(numTimesFound "Interval 4" (func2 $ var))
+
+    --FOR EX3:
+    let dict=[("Prop1",test1),("Prop2",test2),("Prop3",test3),("Prop4",test4) ]
+
+    print(mySort dict)
+    --To check :
+    print(weaker p prop3 prop1)
+    print(weaker p prop1 prop4)
+    print(weaker p prop4 prop2)
+
 
     --- CALL FOR EXERCISE 2 ---------
