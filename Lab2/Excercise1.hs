a4741b2bcc1396781e7f8e30564fc3d8a8511799
stefanovstan <sss5@email.sc.edu>
2022-09-16 18:34:47 +0200

    Exercise 1 comments

--- Lab2/Excercise1.hs
+++ Lab2/Excercise1.hs
@@ -11,13 +11,7 @@ probs n = do
     ps <- probs (n-1)
     return (p:ps)
 
-
-
-
---Step 1: we take func2 which maps  func1 on every element in list x
-func2 x = fmap func1 x
-
---Step 2: We take each element of x and put them into intervals
+--Step 1: We take each element of x and put them into intervals
 func1 :: Float -> [Char]
 func1 n = do
             if  n>0 && n<0.25
@@ -28,15 +22,23 @@ func1 n = do
                 then "Interval 3"
             else "Interval 4"
 
+--Step 2: we take func2 which maps  func1 on every element in list x
+func2 x = fmap func1 x
 
 --Step 3: We count number of times each element occurs in a list
 numTimesFound x xs = (length. filter (== x)) xs
 
+-- When we test for 10000 cases, each quartile is roughly 2500
+{- With larger intereations, we can verify that the probs function properly works and
+    generates an almost equal amount of decimals in each quartile. 
+-}
+
 main :: IO ()
 main = do
-    var <- probs 1000 -- we unwrapped IO float to [Float] using <-
+    var <- probs 100 -- we unwrapped IO float to [Float] using <-
     print(numTimesFound "Interval 1" (func2 $ var))
     print(numTimesFound "Interval 2" (func2 $ var))
     print(numTimesFound "Interval 3" (func2 $ var))
     print(numTimesFound "Interval 4" (func2 $ var))
 
+-- Time spent : 1 hour
