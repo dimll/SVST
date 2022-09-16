f3a17e05108ddf4f38966b1b2e848d42149ac33b
amundlrohne <amundlrohne@gmail.com>
2022-09-16 18:52:22 +0200

    euler comments

--- Lab2/Euler1.hs
+++ Lab2/Euler1.hs
@@ -6,12 +6,21 @@ import System.Random
 import System.IO
 import Data.Function
 
+--This function calculates the numbers that are multiples of 3
 multiple3 :: Int -> [Int]
-multiple3 n = (filter (<n) (map(*3) [1..n]))
+multiple3 n = filter (<n) (map(*3) [1..n])
 
+--This function calculates the numbers that are multiples of 5
 multiple5 :: Int -> [Int]
-multiple5 n = (filter (<n) (map(*5) [1..n]))
+multiple5 n = filter (<n) (map(*5) [1..n])
 
-multiple3or5 n = (multiple3 n) `union` (multiple5 n)
+--This function calculates the numbers that are multiples of 3 or 5. The reason why we used "union" is because we wanted to eliminate duplicates
+-- (AUB) = (A)+ (B) - (AnB)
+multiple3or5 n = multiple3 n `union` multiple5 n
 
+--We add all the list elements up to find the final result.
 res = sum (multiple3or5 1000)
+
+main :: IO () 
+main = do 
+    print res 
--- Lab2/Euler6.hs
+++ Lab2/Euler6.hs
@@ -6,11 +6,19 @@ import System.Random
 import System.IO
 import Data.Function
 
+-- We first calculated n²
 square n =n*n
 squares n= map (square) [1..n]
+-- We then add the list containing n² to find the first part of the difference.
 res1 n = sum (squares n)
 
+-- In reverse order we add all numbers together from our natural numbers list. Then we took the square of the result. That made the second part of the result.
 sumN n = sum [1..n]
 res2 n = square (sumN n)
 
+-- We subtracted the 2 parts to get to the final result.
 diff n = res2 n - res1 n
+
+main :: IO () 
+main = do 
+    print $ diff 100
