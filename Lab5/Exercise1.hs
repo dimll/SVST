92fffe85d2a82425bc0aa6c35eefdc1c030c9c8f
Dimitris <dim-laskos@hotmail.com>
2022-10-07 13:36:04 +0200

    Added extra comments for ex 1

--- Lab5/Exercise1.hs
+++ Lab5/Exercise1.hs
@@ -13,7 +13,9 @@ import Mutation
 -- It breaks property 3, by changing the summatio of elements baes on the element generated randomly. If 0= it does not break
 -- It breaks property 4, the sequence of elements are broken since the same element is added to the both start and end of the list
 -- It breaks property 5, if the number generated modulo input not 0. It dependson the number generated.
-
+-- We must note that there is a case that addElements will generate empty list [] so it will actually 
+-- be identical with the original list. In that case mutate function will return Nothing. 
+-- addElements is the strongest mutator since it covers all the 5 properties. 
 
 -- Remove Elements:
 -- It breaks property 1, because it changes the length
@@ -21,16 +23,21 @@ import Mutation
 -- It breaks property 3, by changing the summation of elements baes on the element generated randomly. If 0= it does not break
 -- It does NOT breaks property 4, the sequence of elements are still the same since we remove at the end of the list, it just with less elements.
 -- It does NOT breaks property 5,the checked numbers are same, just the amount changes.
+-- removeElements is weaker compared to the addElements mutator
 
---Any List:
---It breaks all of the properties.
+-- Any List:
+-- It breaks all of the properties. However it's a "silly" mutator because 
+-- it just generates a random list 
 
 -- Extra Mutators we have written:
+
+-- Alters the value of the first element 
 changeValueOfFirstElement :: [Integer] -> Gen[Integer]
 changeValueOfFirstElement xs = do
   num <- arbitrary :: Gen Integer
   return $ num:tail(xs)
 
+-- Alters the value of a random element 
 changeValueOfAnyElement :: [Integer] -> Gen[Integer]
 changeValueOfAnyElement xs = do
   n <- choose (1, length xs - 1)
@@ -39,6 +46,7 @@ changeValueOfAnyElement xs = do
   num <- (arbitrary :: Gen Integer) `suchThat` (\x -> x /= head xs * toInteger n)
   return (lst1 ++ [num] ++ lst2)
 
+-- Reverses the input list
 reverseList :: [Integer] -> Gen[Integer]
 reverseList xs = do 
   return (reverse xs)
