58bf01ee0b7a05b60b95fcbf4d51ca150f310914
amundlrohne <amundlrohne@gmail.com>
2022-10-13 12:59:36 +0200

    new properties for isSerial

--- Lab6/Exercise4.hs
+++ Lab6/Exercise4.hs
@@ -7,25 +7,10 @@ import SetOrd
 import System.Random
 import Test.QuickCheck
 
-main :: IO ()
-main = do
-  quickCheck qcIsSerial
+(-->) :: Bool -> Bool -> Bool
+p --> q = not p || q
 
 --Exercise 4:
-
---Ex : isSerial [1,2,3,4,5] (tuplesRelnR 4 [1,2,3,4,5])
---In order to prove that RelnR is serial, we test for random domain=random list of integers and random n=random number and check if for all it is serial
-genNumber :: Gen Int
-genNumber = (arbitrary :: Gen Int) `suchThat` (> 0)
-
-genNumberList :: Gen [Int]
-genNumberList = listOf genNumber `suchThat` (not . null)
-
-qcIsSerial :: Gen Bool
-qcIsSerial = do
-  n <- genNumber
-  nLs <- genNumberList
-  return $ isSerial nLs (tuplesRelnR n nLs)
 
 --PART A:
 --Every element "x" in the domain must be in relation (x,y)
@@ -37,40 +22,40 @@ isSerial :: (Eq a, Ord a) => [a] -> Rel a -> Bool
 isSerial domain rl = (domain == nub (map fst rl)) && all (\(_, x) -> x `elem` domain) rl
 
 --PART B:
-createReflexive :: a -> (a, a)
-createReflexive a = (a, a)
-
-isReflexive :: (Eq a, Ord a) => [a] -> Rel a -> Bool
-isReflexive domainA rl = list2set (filter (`inSet` list2set (map createReflexive domainA)) rl) == list2set (map createReflexive domainA)
-
---Ex irreflexive : isIrReflexive [1,2,3] [(1,2),(1,1),(2,2)] returns True
---QUICKCHECKPROP1 : Check if reflexive. All reflexive relations are serial
--- Information about serial relations obtained from : http://users.cecs.anu.edu.au/~jks/LogicNotes/relations.html
-isIrReflexive :: (Eq a, Ord a) => [a] -> Rel a -> Bool
-isIrReflexive domainA rl = not (isReflexive domainA rl)
-
-isSymmetric :: (Eq a, Ord a) => Rel a -> Bool
-isSymmetric rl = subSet (list2set (symClos rl)) (list2set rl)
-
---QUICKCHECKPROP2 : Check if antisymmetric (may not be complete)
---In order to be antisymmetric = irreflexive + not symmetric
---isAntisymmetric domainA rl = if ((isIrReflexive domainA rl) && (not(isSymmetric rl))) then True else False
-
---QUICKCHECKPROP2 : The length of tuples's in [(a,b)] must be at least the length of the domain
-lengthProperty domainA rl = length domainA <= length rl
+isReflexive :: (Ord a) => [a] -> Rel a -> Bool
+isReflexive domain rel = all (\x -> (x, x) `elem` rel) domain
 
 --PART C
-funcR :: Int -> [Int] -> [Int]
-funcR n = map (mod n)
+relR :: Int -> [Int] -> [Int]
+relR n = map (`mod` n)
 
-relnR :: Int -> [Int] -> [Int]
-relnR n domainR = funcR n domainR
+--Creates the relation R:
+tuplesRelR :: Int -> [Int] -> [(Int, Int)]
+tuplesRelR n domainR = zip domainR (relR n domainR)
 
---Creates the reln R:
-tuplesRelnR :: Int -> [Int] -> [(Int, Int)]
-tuplesRelnR n domainR = zip domainR (relnR n domainR)
+--Ex : isSerial [1,2,3,4,5] (tuplesRelnR 4 [1,2,3,4,5])
+--In order to prove that RelnR is serial, we test for random domain=random list of integers and random n=random number and check if for all it is serial
+genNumber :: Gen Int
+genNumber = (arbitrary :: Gen Int) `suchThat` (> 0)
 
---https://gist.github.com/thekarel/9964975
---applyNtimes :: (Num n, Ord n) => n -> (a -> a) -> a -> a
---applyNtimes f 1 = main
---applyNtimes f n =(applyNtimes (n-1) main)
+genNumberList :: Gen [Int]
+genNumberList = listOf genNumber `suchThat` (not . null)
+
+-- In order for the relation R to be serial, n cannot be divisible by any of the elements in the domain.
+-- An easy way to check this is to make sure that n is greater than the largest x in the domain (maximum).
+-- This means, that if n is divisible with any of the elements in the domain, R is not serial.
+qcIsSerial :: Gen Bool
+qcIsSerial = do
+  nLs <- genNumberList
+  n <- genNumber `suchThat` (\x -> x > maximum nLs)
+  return $ isSerial (nub nLs) (tuplesRelR n (nub nLs))
+
+propReflexiveIsSerial :: Ord a => [a] -> Rel a -> Bool
+propReflexiveIsSerial domain rel = isReflexive domain rel --> isSerial domain rel
+
+propSerialLength :: Ord a => [a] -> Rel a -> Bool
+propSerialLength domainA rl = length domainA <= length rl
+
+main :: IO ()
+main = do
+  quickCheck qcIsSerial
