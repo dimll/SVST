2b9f33b3794b6f89360935b6e5a737226d21b3bb
Dimitris <dim-laskos@hotmail.com>
2022-10-13 15:02:32 +0200

    Exercise 1 completed

--- Lab6/Exercise1.hs
+++ Lab6/Exercise1.hs
@@ -9,6 +9,10 @@ import System.Random
 import Test.QuickCheck
 
 -- Generator using QuickCheck module
+-- We first generate a random positive number l 
+-- that will be the length of the list and after 
+-- that we define that each number of the list must be 
+-- positive (to be in accordance with natural numbers)
 genSet ::  Gen  (Set Int)
 genSet = do 
     l <- arbitrary `suchThat` (>0)
@@ -16,17 +20,23 @@ genSet = do
     return $ list2set n
 
 -- Random generation without QuickCheck
-genRandomInt :: IO Int 
-genRandomInt = randomRIO (1,100) :: IO Int
-
--- Source: https://stackoverflow.com/questions/30740366/list-with-random-numbers-in-haskell
+-- Similar with the previous implementation we are generating 
+-- first the list length (with a maximum length of 100) and after 
+-- that we are generating random numbers for the list 
 genSetWithoutQ :: IO (Set Int) 
 genSetWithoutQ = do
     l <- genRandomInt
     rNum <- sequence $ replicate l $ randomRIO(1,100::Int)
     return $ Set rNum 
 
+-- Helper function: generates a number between [1,100]
+genRandomInt :: IO Int 
+genRandomInt = randomRIO (1,100) :: IO Int
 
+-- We have used the [sequence $ replicate l $ ..] command 
+-- from https://stackoverflow.com/questions/30740366/list-with-random-numbers-in-haskell
+
+-- Examples of the two test generators
 main :: IO ()
 main = do 
     -- Example without QuickCheck
