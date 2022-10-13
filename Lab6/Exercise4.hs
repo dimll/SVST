718d9be79596e7f0804d9470244cee20afe385f8
Selin99 <48292786+Selin99@users.noreply.github.com>
2022-10-13 10:12:27 +0200

    Add files via upload
    Exercise 4

---
+++ Lab6/Exercise4.hs
@@ -0,0 +1,74 @@
+module Exercise4 where
+import Test.QuickCheck
+import System.Random
+import Exercise2
+import Exercise3
+import SetOrd
+import Data.List
+
+main :: IO ()
+main = do
+       quickCheck qcIsSerial
+
+--Exercise 4:
+
+genNumber :: Gen Int 
+genNumber = (arbitrary :: Gen Int) `suchThat` (> 0)
+
+genNumberList :: Gen [Int]
+genNumberList = listOf genNumber `suchThat` (\x -> length x > 0)
+
+qcIsSerial :: Gen Bool 
+qcIsSerial = do 
+       n <- genNumber
+       nLs <- genNumberList
+       return $ isSerial nLs (tuplesRelnR n nLs)
+
+--PART A: 
+--Every element "x" in the domain must be in relation (x,something)
+--An example to serial relations : successor function
+-- Serial relations are : irreflexive, antisymmetric
+
+isSerial :: (Eq a, Ord a) => [a] -> Rel a -> Bool
+isSerial domainA rl = if list2set (domainA) == list2set(nub(map(fst) rl)) then True else False
+
+--PART B:
+createReflexive :: a -> (a,a)
+createReflexive a = (a,a)
+
+isReflexive :: (Eq a, Ord a) => [a] -> Rel a -> Bool
+isReflexive domainA rl = if list2set(filter (`inSet`  (list2set(map(createReflexive) domainA)) ) rl) == (list2set(map(createReflexive) domainA)) then True else False
+
+--Ex irreflexive : isIrReflexive [1,2,3] [(1,2),(1,1),(2,2)] returns True
+--QUICKCHECKPROP1 : Check if reflexive. All reflexive relations are serial
+-- Information about serial relations obtained from : http://users.cecs.anu.edu.au/~jks/LogicNotes/relations.html
+isIrReflexive ::(Eq a, Ord a) => [a] -> Rel a -> Bool
+isIrReflexive domainA rl = if not(isReflexive domainA rl) then True else False
+
+isSymmetric ::(Eq a, Ord a) => Rel a -> Bool
+isSymmetric rl =  if subSet (list2set (symClos rl)) (list2set (rl)) then True else False
+
+--QUICKCHECKPROP2 : Check if antisymmetric (may not be complete)
+--In order to be antisymmetric = irreflexive + not symmetric
+--isAntisymmetric domainA rl = if ((isIrReflexive domainA rl) && (not(isSymmetric rl))) then True else False
+
+--QUICKCHECKPROP2 : The length of tuples's in [(a,b)] must be at least the length of the domain
+lengthProperty domainA rl = if (length domainA) <= (length rl) then True else False
+
+--PART C
+funcR :: Int -> [Int] -> [Int]
+funcR n y =map (mod n) y
+relnR n domainR = funcR n domainR
+
+--Creates the reln R:
+tuplesRelnR n domainR= (zip (domainR) (relnR n domainR))
+--Ex : isSerial [1,2,3,4,5] (tuplesRelnR 4 [1,2,3,4,5])
+--In order to prove that RelnR is serial, we test for random domain=random list of integers and random n=random number and check if for all it is serial
+genN = arbitrary::Gen Int
+genListN = arbitrary :: Gen [Int]
+
+--https://gist.github.com/thekarel/9964975
+--applyNtimes :: (Num n, Ord n) => n -> (a -> a) -> a -> a
+--applyNtimes f 1 = main
+--applyNtimes f n =(applyNtimes (n-1) main)
+
